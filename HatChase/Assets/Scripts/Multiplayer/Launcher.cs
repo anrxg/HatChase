3a0d12fe3c760fea89ed0da08c24919520085859
anrxg <shuklaking676767@gmail.com>
2025-07-02 00:36:54 +0530

    Created loading and Homepage Menu

--- HatChase/Assets/Scripts/Multiplayer/Launcher.cs
+++ HatChase/Assets/Scripts/Multiplayer/Launcher.cs
@@ -3,5 +3,27 @@ using Photon.Pun;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
-   
+    void Start()
+    {
+        Debug.Log("Connecting To Master...");
+        PhotonNetwork.ConnectUsingSettings();
+        MenuManager.instance.OpenMenu("LoadingMenu");
+    }
+
+    #region Overrides
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected To Matser!");
+        OnJoinedLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        MenuManager.instance.OpenMenu("TitleMenu");
+        Debug.Log("Lobby Joined...");
+    }
+    #endregion
+
+
 }
---
+++ HatChase/Assets/Scripts/Multiplayer/Menu/Menu.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public string menuName;
+    public bool isOpen;
+    public void Open()
+    {
+        gameObject.SetActive(true);
+        isOpen = true;
+    }
+
+    public void Close()
+    {
+        gameObject.SetActive(false);
+        isOpen = false;
+    }
+}
